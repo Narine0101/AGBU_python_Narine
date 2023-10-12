04cc43c19a7da23daf7f9ad7dcda0202895f6659
Gevorg <kasarjyang@gmail.com>
2023-10-12 08:29:01 +0400

    Add Variables

---
+++ variables.py
@@ -0,0 +1,66 @@
+
+
+
+
+
+
+x = 5
+y = "aa"
+print(x)
+print(y)
+print(x,y)
+print(str(x)+ " " +y)
+
+x = 4
+x = "aaa"
+print(x)
+
+
+x = str(3)
+y = int(3)
+z = float(3)
+print(x, y, z)
+
+print(type(x), type(y), type(z))
+
+
+x = 4, 5, 6
+green, yello, red = x
+print(green)
+print(yello)
+print(red)
+
+print(yello, green, red)
+
+x, y, z = 4, 5, 6
+print(x, y, z)
+
+x = y = z = 5
+print(x, y, z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x, y, z)
+print(fruits)
+
+fruits = ("apple", "banana", "cherry")
+x, y, z = fruits
+print(x, y, z)
+print(fruits)
+
+fruits = "apple", "banana", "cherry"
+x, y, z = fruits
+print(x, y, z)
+print(fruits)
+
+tiv = ("1", "2", "3", "4", "5")
+(yello, gree, *red) = tiv
+print(yello)
+print(gree)
+print(red)
+
+x = -4
+y = -2
+z = x**y
+print(z)
+
