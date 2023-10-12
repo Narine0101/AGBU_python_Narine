1790deb217dc29a391fb5b5fa84b6cf67fe268f1
Narine0101 <nsahakyan01@gmail.com>
2023-10-12 09:10:31 +0400

    String

---
+++ string_Narine_Sahakyan.py
@@ -0,0 +1,8 @@
+#  1. Write a program that get user input and return  every third element from inputed string.
+# txt = input("Please input txt")
+# print("Every third element from input string is:", txt[::3])
+
+
+#   2. Write a program that get user input and return  every second element from inputed string from end.
+txt = input("Please input txt")
+print("Every second element from input string from end is:", txt[::-2])
